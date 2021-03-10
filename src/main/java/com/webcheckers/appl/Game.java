f9134eb9196e77765015de63ecba362a79d19318
Michael Merlino <mtm1783@rit.edu>
2021-03-09 21:29:41 -0500

    Added more to Game.java, including helper methods for the "droppable" functionality

--- src/main/java/com/webcheckers/appl/Game.java
+++ src/main/java/com/webcheckers/appl/Game.java
@@ -16,7 +16,7 @@ public class Game {
      * Create a new standard board based on checkers rules.
      *
      * @return
-     *    the new board, as an array of piece enums
+     *    The new board, as an array of piece enums
      */
     private Piece[][] newBoardSetup()
     {
@@ -27,58 +27,16 @@ public class Game {
             for (int col = 0; col < 8; col++)
             {
                 // White side (top)
-                if (row <= 2)
+                if (row <= 2 && isWhiteTile(row, col))
                 {
-                    if (row % 2 == 0) // From the top, rows 0 and 2
-                    {
-                        if (col % 2 == 0) // Even columns
-                        {
-                            newBoard[row][col] = Piece.None;
-                        }
-                        else // Odd columns
-                        {
-                            newBoard[row][col] = Piece.White;
-                        }
-                    }
-                    else // Row 1
-                    {
-                        if (col % 2 == 0) // Even columns
-                        {
-                            newBoard[row][col] = Piece.White;
-                        }
-                        else // Odd columns
-                        {
-                            newBoard[row][col] = Piece.None;
-                        }
-                    }
+                    board[row][col] = Piece.White;
                 }
                 // Red side (bottom)
-                else if (row >= 5)
+                else if (row >= 5 && isWhiteTile(row, col))
                 {
-                    if (row % 2 == 0) // From the top, rows 5 and 7
-                    {
-                        if (col % 2 == 0) // Even columns
-                        {
-                            newBoard[row][col] = Piece.None;
-                        }
-                        else // Odd columns
-                        {
-                            newBoard[row][col] = Piece.Red;
-                        }
-                    }
-                    else // Row 6
-                    {
-                        if (col % 2 == 0) // Even columns
-                        {
-                            newBoard[row][col] = Piece.Red;
-                        }
-                        else // Odd columns
-                        {
-                            newBoard[row][col] = Piece.None;
-                        }
-                    }
+                    board[row][col] = Piece.Red;
                 }
-                else // Other rows not accounted for, e.g. 3 and 4
+                else // Center field
                 {
                     newBoard[row][col] = Piece.None;
                 }
@@ -88,11 +46,56 @@ public class Game {
         return newBoard;
     }
 
-    /**
-     * Returns the current game board.
-     */
     public Piece[][] getBoard()
     {
         return this.board;
+    }
+
+    /**
+     * Checks if a tile is occupied by a checker
+     * @param x
+     *      The X coordinate of the tile
+     * @param y
+     *      The Y coordinate of the tile
+     * @return
+     *      True if there is no checker on the tile, false if there is one
+     */
+    private boolean isTileEmpty(int x, int y)
+    {
+        return board[x][y] != Piece.None;
+    }
+
+    /**
+     * Given the x,y coordinates of a piece and a target location, determines if the piece can be dropped at the target.
+     * @param pieceX
+     *      The X location of the selected piece
+     * @param pieceY
+     *      The Y location of the selected piece
+     * @param targetX
+     *      The X location of the target tile
+     * @param targetY
+     *      The Y location of the target tile
+     * @return
+     *      True if the piece can be dropped at the target, false is not
+     */
+    public boolean isDroppable(int pieceX, int pieceY, int targetX, int targetY)
+    {
+        // Is the target tile white or occupied? If so, not droppable
+        // Is the tile where the selected piece should be actually occupied by a piece? If not, not droppable
+        return isWhiteTile(targetX, targetY) && isTileEmpty(targetX, targetY) && !isTileEmpty(pieceX, pieceY);
+    }
+
+    /**
+     * Returns whether or not a tile on the checker board is white
+     * @param x
+     *      The X coordinate of the tile
+     * @param y
+     *      The Y coordinate of the tile
+     * @return
+     *      True if the tile is while, false if the tile is black
+     */
+    private boolean isWhiteTile(int x, int y)
+    {
+        return (x + y) % 2 != 0;
     }
 }
