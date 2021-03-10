f27c68759da88a52e962c593b276c2d8137a81cd
Alex Johannesson <arj6153@rit.edu>
2021-03-10 18:42:07 -0500

    Piece class setup and edited so that pieces are correctly instantiated.

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -1,7 +1,24 @@
 package com.webcheckers.model;
 
-public enum Piece {
-    White,
-    Red,
-    None
+public class Piece {
+    public enum color{Red, White, None}
+    public enum pieceType{Single, King}
+    private color Color;
+    private pieceType Type;
+
+
+    public Piece(color Color) {
+        Type = pieceType.Single;
+        this.Color = Color;
+    }
+
+
+    public pieceType getType() {
+        return Type;
+    }
+
+    public color getColor() {
+        return Color;
+    }
+
 }
