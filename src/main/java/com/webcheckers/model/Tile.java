93f21634378dadd8f6c6037ab11d98b1d15b6223
Truong Anh Tuan Hoang <tath2701@gmail.com>
2021-03-11 08:59:26 -0500

    adding doc string s

--- src/main/java/com/webcheckers/model/Tile.java
+++ src/main/java/com/webcheckers/model/Tile.java
@@ -1,11 +1,18 @@
 package com.webcheckers.model;
-
+/**
+ * Class containing the Tiles of the checker board.
+ */
 public class Tile {
 
     private int cellIdx;
     private Piece piece;
     private boolean valid;
 
+    /**
+     * Constructor of Tile.
+     * @param cellIdx cell information
+     * @param currentPiece current checker piece
+     */
     public Tile(int cellIdx, Piece currentPiece, boolean valid) {
         this.valid = valid;
         this.cellIdx = cellIdx;
@@ -15,12 +22,19 @@ public class Tile {
             this.piece = currentPiece;
         }
     }
-
+    /**
+     * Gets cell column.
+     * @return column number
+     */
     public int getCellIdx() {
         return this.cellIdx;
     }
 
-
+    /**
+     * Sets a checker piece on an empty tile.
+     * @param piece current checker piece
+     * @return boolean if successful set or not
+     */
     public boolean setPiece(Piece piece) {
         if (this.piece == null) {
             this.piece = piece;
@@ -28,13 +42,20 @@ public class Tile {
         }
         return false;
     }
-
+    /**
+     * Checks if the tile is valid.
+     * @return true if the tile is black and there is not piece already there,
+     *         otherwise false
+     */
     public boolean isValid() {
         return valid && piece == null;
 
     }
 
-
+    /**
+     * Checks status of a tile to see if it is occupied.
+     * @return true if tile is occupied, else false
+     */
     public Piece getPiece() {
         return this.piece;
     }
