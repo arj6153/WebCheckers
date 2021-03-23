400b43022c9347c5cce15d62bb6f18fa6eac3a07
Alex Johannesson <arj6153@rit.edu>
2021-03-22 21:19:24 -0400

    lol

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -9,6 +9,7 @@ public class Space {
     private int cellIdx;
     private Piece piece;
     private boolean valid;
+    private boolean urmom;
 
     /**
      * Constructor of Tile.
@@ -22,6 +23,7 @@ public class Space {
         this.valid = valid;
         this.cellIdx = cellIdx;
         this.piece = currentPiece;
+        this.urmom = urmom;
     }
     /**
      * Gets cell column.
