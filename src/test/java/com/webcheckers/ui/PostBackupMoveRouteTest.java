45bdc571aa4046a3ec65c9ce97fd847f8bed53de
Alex Johannesson <arj6153@rit.edu>
2021-03-30 09:13:32 -0400

    Test cases implemented for backupmove and checkturn

---
+++ src/test/java/com/webcheckers/ui/PostBackupMoveRouteTest.java
@@ -0,0 +1,74 @@
+package com.webcheckers.ui;
+
+import com.google.gson.Gson;
+import com.webcheckers.appl.Game;
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Ash Nguyen
+ */
+@Tag("UI-tier")
+class PostBackupMoveRouteTest {
+    private PostBackupMoveRoute BackUpMoveRoute;
+
+    private GameCenter gameCenter;
+    private Game game;
+    private BoardView board;
+    private static final String PLAYER1 = "test";
+    private static final String PLAYER2 = "opponent";
+    private Player player;
+    private Player opponent;
+    private Request request;
+    private Session session;
+    private Response response;
+    private Gson gson;
+    private String gameID;
+
+    @BeforeEach
+    public void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        gson = new Gson();
+
+        gameCenter = new GameCenter();
+        gameCenter.addPlayer(PLAYER1);
+        gameCenter.addPlayer(PLAYER2);
+        gameID = UUID.randomUUID().toString();
+        gameCenter.addGame(gameCenter.getPlayer(PLAYER1), gameCenter.getPlayer(PLAYER2));
+        player = gameCenter.getPlayer(PLAYER1);
+        player.setPlaying(true);
+
+        opponent = gameCenter.getPlayer(PLAYER2);
+        opponent.setPlaying(true);
+
+        //creating and adding the game with gameID into the game center
+        gameID = UUID.randomUUID().toString();
+        gameCenter.addGame(player, opponent);
+        game = gameCenter.getGame(Integer.parseInt(gameID));
+        when(request.queryParams(GetGameRoute.GAMEID_ATTR)).thenReturn(gameID);
+
+        BackUpMoveRoute = new PostBackupMoveRoute(gameCenter, gson);
+    }
+
+    @Test
+    public void backupSuccess() throws Exception {
+
+        if (game.undoMove() == true)
+            when(request.session().attribute(GetHomeRoute.CURRENT_USER)).thenReturn(PLAYER1);
+        BackUpMoveRoute.handle(request, response);
+    }
+}
---
+++ src/test/java/com/webcheckers/ui/PostCheckTurnRouteTest.java
@@ -0,0 +1,74 @@
+
+package com.webcheckers.ui;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.$Gson$Preconditions;
+import com.webcheckers.appl.Game;
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.model.Row;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+import spark.TemplateEngine;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class PostCheckTurnRouteTest {
+
+    private PostCheckTurnRoute CheckTurnRoute;
+
+    private static final String PLAYER1 = "test";
+    private static final String PLAYER2 = "opponent";
+    private GameCenter gameCenter;
+    private String gameID;
+    private Game game;
+
+    private Request request;
+    private Response response;
+    private Session session;
+    private Gson gson;
+
+    @BeforeEach
+    public void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        gson = new Gson();
+        gameCenter = new GameCenter();
+        gameCenter.addPlayer(PLAYER1);
+        gameCenter.addPlayer(PLAYER2);
+        gameID = UUID.randomUUID().toString();
+        gameCenter.addGame(gameCenter.getPlayer(PLAYER1), gameCenter.getPlayer(PLAYER2));
+        game = gameCenter.getGame(Integer.parseInt(gameID));
+        when(request.session().attribute(GetHomeRoute.CURRENT_USER)).thenReturn(gameCenter.getPlayer(PLAYER1));
+        when(request.queryParams(GetGameRoute.GAMEID_ATTR)).thenReturn(gameID);
+        CheckTurnRoute = new PostCheckTurnRoute(gameCenter, gson);
+    }
+
+    @Test
+    public void isGameOver() {
+        game.resignGame(gameCenter.getPlayer(PLAYER1));
+        CheckTurnRoute.handle(request,response);
+    }
+
+    @Test
+    public void checkTurnPlayer1() {
+        game.setPlayerTurn(gameCenter.getPlayer(PLAYER1));
+        CheckTurnRoute.handle(request,response);
+    }
+
+    @Test
+    public void checkTurnPlayer2() {
+        game.setPlayerTurn(gameCenter.getPlayer(PLAYER2));
+        CheckTurnRoute.handle(request,response);
+    }
+
+}
